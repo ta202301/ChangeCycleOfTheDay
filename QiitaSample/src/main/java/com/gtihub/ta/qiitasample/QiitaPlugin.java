07947ff3b576ed75558212879462245341557889
ta202301 <mizuho.takada.c@gmail.com>
2023-05-04 12:10:37 +0900

    testcoding

---
+++ QiitaSample/src/main/java/com/gtihub/ta/qiitasample/QiitaPlugin.java
@@ -0,0 +1,11 @@
+package com.gtihub.ta.qiitasample;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class QiitaPlugin extends JavaPlugin{
+    //↓ onEnableはロードされた時に実行されるメソッド
+    @Override
+    public void onEnable() {
+        // ↓ サーバー上にログを残す
+        getLogger().info("Hello, Qiita!");
+    }
+}
